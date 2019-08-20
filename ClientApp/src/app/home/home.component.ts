a2f0d6de66e8123e2a4150d75d97dc2520187c50
kanor pokpok <kanorpokpok@gmail.com>
2019-08-20 20:19:37 +0700

    ee

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  
+  constructor(private http: HttpClient,@Inject('BASE_URL') private baseUrl: string) {}
+
+  ngOnInit() {
+    this.http.get<any>(`ws://${this.baseUrl}api/Stream`).subscribe(
+      data => {
+      console.log(data);
+    }, error => console.error(error));
+  }
+  
 }
